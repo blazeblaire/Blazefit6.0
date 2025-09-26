22a5acc94945f3b2d93b7e5f4cb2b381f1c9f8ab
TONNY <blare9.70@gmail.com>
2025-09-26 23:07:04 +0300

    fixed scope issues with plan variable

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-const base_url = 'http://localhost:3000/';
-const workoutPlan = document.querySelector('.workout-plan');
-const btns = document.getElementsByClassName('buttons')
-const bT = document.querySelectorAll('button')  // grab all the buttons
-
-
 document.addEventListener('DOMContentLoaded', () => {
+    // const base_url = 'http://localhost:3000/';
+    const workoutPlan = document.querySelector('.workout-plan');
+    const bT = document.querySelectorAll('button')  // grab all the buttons
     //get workout plans from json
-    fetch(`${base_url}plans.json`)
+    let plan = {};
+    fetch('http://localhost:3000/plans')
         .then(response => response.json())
         .then(data => {
-            const plan = data;
+            plan = data;
             console.log(plan);
 
         })
@@ -25,39 +23,37 @@ document.addEventListener('DOMContentLoaded', () => {
             const mood = button.getAttribute("data-mood");
             console.log(mood)
             // function to display workout
-            disworkout(mood)
+            display(mood);
         })
 
-    });
+        function display(mood) {
 
-    function disworkout(mood) {
-        // clear old content
-        workoutPlan.innerHTML = ""
-        // title goes here
-        const rendPlan = plan[mood];
-        const title = document.createElement('h2');
-        title.textContent = rendPlan.feedback;
-        workoutPlan.appendChild(title);
+            // clear old content
+            workoutPlan.innerHTML = ""
+            // title goes here
+            const rendPlan = plan[mood];
+            const title = document.createElement('h2');
+            title.textContent = rendPlan.feedback;
+            workoutPlan.appendChild(title);
 
-        //section containing the actual plans
-        for (const sectionName in rendPlan.section) {
-            const sectionTitle = document.createElement('h3');
-            sectionTitle.textContent = sectionName;
-            workoutPlan.appendChild(sectionTitle);
+            //section containing the actual plans
+            for (const sectionName in rendPlan.section) {
+                const sectionTitle = document.createElement('h3');
+                sectionTitle.textContent = sectionName;
+                workoutPlan.appendChild(sectionTitle);
 
-            const list = document.createElement('ul');
-            plan.section[sectionName].forEach(item => {
-                const li = document.createElement('li');
-                li.textContent = item;
-                list.appendChild(li);
-            });
+                const list = document.createElement('ul');
+                rendPlan.section[sectionName].forEach(item => {
+                    const li = document.createElement('li');
+                    li.textContent = item;
+                    list.appendChild(li);
+                });
 
-            workoutPlan.appendChild(list);
+                workoutPlan.appendChild(list);
+            }
+
         }
-    }
-
-
-
+    });
 })
 
 
